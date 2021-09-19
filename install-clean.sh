6f0c71243be46e0bf4ce318e266299a86f16d846
DanielPalmSie <palm6991@gmail.com>
2021-09-20 02:27:29 +0300

    delete some files

--- install-clean.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-php bin/console doc:data:drop --if-exists --force
-php bin/console doc:data:crea
-php bin/console doc:sch:crea
--- install-import-fixtures.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-php bin/console doc:data:drop --if-exists --force
-php bin/console doc:data:crea
-php bin/console doc:sch:crea
-php bin/console doctrine:fixtures:load --env=dev --no-interaction
